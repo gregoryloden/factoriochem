57eedfd701b3e662cab7fa3511aeba9368c820f7
Gregory Loden <gregory.loden@gmail.com>
2023-03-29 12:11:51 -0400

    build the gui from nested spec tables

--- control/gui.lua
+++ control/gui.lua
@@ -4,6 +4,14 @@ function close_gui(gui)
 	end
 end
 
+function gui_add_recursive(gui, element_spec)
+	local children_spec = element_spec.children
+	element_spec.children = nil
+	local element = gui.add(element_spec)
+	if not children_spec then return end
+	for _, child_spec in ipairs(children_spec) do gui_add_recursive(element, child_spec) end
+end
+
 function on_gui_opened(event)
 	local entity = event.entity
 	if not (entity and entity.valid) then return end
@@ -11,7 +19,36 @@ function on_gui_opened(event)
 
 	local gui = game.get_player(event.player_index).gui
 	close_gui(gui)
-	local outer = gui.relative.add({
+
+	function build_molecule_spec(name)
+		return {
+			type = "sprite-button",
+			name = name,
+			style = "factoriochem-poc-big-slot-button",
+			sprite = "item/O1-H|1H",
+		}
+	end
+	function build_reaction_table_spec(name_prefix, transition_spec)
+		return {
+			type = "table",
+			name = name_prefix.."table",
+			column_count = 3,
+			children = {
+				build_molecule_spec(name_prefix.."base"),
+				{type = "empty-widget"},
+				build_molecule_spec(name_prefix.."result"),
+				build_molecule_spec(name_prefix.."catalyst"),
+				transition_spec,
+				build_molecule_spec(name_prefix.."bonus"),
+				build_molecule_spec(name_prefix.."modifier"),
+				{type = "empty-widget"},
+				build_molecule_spec(name_prefix.."remainder"),
+			},
+		}
+
+	end
+	local gui_spec = {
+		-- outer
 		type = "frame",
 		caption = {"factoriochem-poc.reaction"},
 		name = "molecule-rotater",
@@ -19,47 +56,32 @@ function on_gui_opened(event)
 			gui = defines.relative_gui_type.assembling_machine_gui,
 			position = defines.relative_gui_position.right
 		},
-	})
-	local inner = outer.add({type = "flow", style = "inset_frame_container_vertical_flow", direction = "vertical"})
-	local reaction_frame = inner.add({type = "frame", style = "b_inner_frame"})
-	local reaction_table = reaction_frame.add({type = "table", name = "reaction-table", column_count = 3})
-	for _, name in ipairs({"base", "", "result", "catalyst", "description", "bonus", "modifier", "", "remainder"}) do
-		if name == "" then
-			reaction_table.add({type = "empty-widget"})
-		elseif name == "description" then
-			reaction_table.add({
-				type = "label",
-				caption = " > Results > ",
-			})
-		else
-			reaction_table.add({
-				type = "sprite-button",
-				name = "reaction-"..name,
-				style = "factoriochem-poc-big-slot-button",
-				sprite = "item/O1-H|1H",
-			})
-		end
-	end
-	local reaction_demo_frame = inner.add({type = "frame", style = "b_inner_frame", direction = "vertical"})
-	reaction_demo_frame.add({type = "label", caption = "Demo:"})
-	local reaction_demo_table = reaction_demo_frame.add({type = "table", name = "reaction-demo-table", column_count = 3})
-	for _, name in ipairs({"base", "", "result", "catalyst", "description", "bonus", "modifier", "", "remainder"}) do
-		if name == "" then
-			reaction_demo_table.add({type = "empty-widget"})
-		elseif name == "description" then
-			reaction_demo_table.add({
-				type = "label",
-				caption = " > Results > ",
-			})
-		else
-			reaction_demo_table.add({
-				type = "sprite-button",
-				name = "reaction-demo-"..name,
-				style = "factoriochem-poc-big-slot-button",
-				sprite = "item/O1-H|1H",
-			})
-		end
-	end
+		children = {{
+			-- inner
+			type = "flow",
+			style = "inset_frame_container_vertical_flow",
+			direction = "vertical",
+			children = {{
+				-- reaction frame
+				type = "frame",
+				style = "b_inner_frame",
+				children = {
+					build_reaction_table_spec("reaction-", {type = "label", caption = " > Results > "}),
+				},
+			}, {
+				-- reaction demo frame
+				type = "frame",
+				style = "b_inner_frame",
+				direction = "vertical",
+				children = {
+					{type = "label", caption = "Demo:"},
+					build_reaction_table_spec(
+						"reaction-demo-", {type = "label", caption = " > Results > "}),
+				},
+			}},
+		}},
+	}
+	gui_add_recursive(gui.relative, gui_spec)
 end
 
 function on_gui_closed(event)
