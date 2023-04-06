ec90f381b6c2ff7cab0a01111949be4f921162f1
Gregory Loden <gregory.loden@gmail.com>
2023-04-06 14:02:48 -0400

    remove the item selection from demo reactants, instead copy from the reaction table or player cursor

--- control/gui.lua
+++ control/gui.lua
@@ -77,14 +77,20 @@ local function demo_reaction(building_data, demo_state, reaction_demo_table)
 		demo_state.products[product_name] = nil
 	end
 	local building_definition = BUILDING_DEFINITIONS[building_data.entity.name]
-	local has_all_selectors = true
-	for reactant_name, _ in pairs(building_definition.selectors) do
-		if not demo_state.selectors[reactant_name] then
-			has_all_selectors = false
+	local valid_reaction = true
+	for _, reactant in pairs(demo_state.reactants) do
+		if game.item_prototypes[reactant].group.name ~= MOLECULES_GROUP_NAME then
+			valid_reaction = false
 			break
 		end
 	end
-	if has_all_selectors then building_definition.reaction(demo_state) end
+	for reactant_name, _ in pairs(building_definition.selectors) do
+		if not demo_state.selectors[reactant_name] then
+			valid_reaction = false
+			break
+		end
+	end
+	if valid_reaction then building_definition.reaction(demo_state) end
 	for _, product_name in ipairs(building_definition.products) do
 		local element = reaction_demo_table[REACTION_DEMO_PREFIX..product_name]
 		local product = demo_state.products[product_name]
@@ -94,6 +100,16 @@ local function demo_reaction(building_data, demo_state, reaction_demo_table)
 			element.sprite = nil
 		end
 	end
+end
+
+local function demo_reaction_with_reactant(building_data, demo_state, element, reactant_name, reactant)
+	demo_state.reactants[reactant_name] = reactant
+	if reactant then
+		element.sprite = "item/"..reactant
+	else
+		element.sprite = nil
+	end
+	demo_reaction(building_data, demo_state, element.parent)
 end
 
 
@@ -121,14 +137,9 @@ local function on_gui_opened(event)
 		elseif name_prefix == REACTION_DEMO_PREFIX then
 			if is_reactant then
 				spec.tooltip = {"factoriochem-poc.reaction-demo-table-reactant-tooltip"}
-				spec.type = "choose-elem-button"
-				spec.elem_type = "item"
-				local filters = {}
-				for _, subgroup in ipairs(game.item_group_prototypes[MOLECULES_GROUP_NAME].subgroups) do
-					table.insert(filters, {filter = "subgroup", subgroup = subgroup.name})
+				if demo_state.reactants[component_name] then
+					spec.sprite = "item/"..demo_state.reactants[component_name]
 				end
-				spec.elem_filters = filters
-				spec.item = demo_state.reactants[component_name]
 			elseif demo_state.products[component_name] then
 				spec.sprite = "item/"..demo_state.products[component_name]
 			end
@@ -221,10 +232,10 @@ local function on_gui_click(event)
 	local element = event.element
 	local building_data = global.molecule_reaction_building_data[global.current_gui_entity[event.player_index]]
 
-	local reaction_table_component = REACTION_TABLE_COMPONENT_NAME_MAP[element.name]
-	if reaction_table_component then
+	local reaction_table_component_name = REACTION_TABLE_COMPONENT_NAME_MAP[element.name]
+	if reaction_table_component_name then
 		local player = game.get_player(event.player_index)
-		local chest_inventory = building_data.chest_inventories[reaction_table_component]
+		local chest_inventory = building_data.chest_inventories[reaction_table_component_name]
 		local chest_contents = chest_inventory.get_contents()
 		if next(chest_contents) then
 			local player_inventory = player.get_main_inventory()
@@ -233,9 +244,27 @@ local function on_gui_click(event)
 				if added > 0 then chest_inventory.remove({name = name, count = added}) end
 			end
 			update_reaction_table_sprite(element, chest_inventory)
-		elseif player.cursor_stack then
+		elseif player.cursor_stack and player.cursor_stack.valid_for_read then
 			chest_inventory.find_empty_stack().transfer_stack(player.cursor_stack)
 			update_reaction_table_sprite(element, chest_inventory)
+		end
+		return
+	end
+
+	local reaction_demo_table_reactant_name = REACTION_DEMO_TABLE_REACTANT_NAME_MAP[element.name]
+	if reaction_demo_table_reactant_name then
+		local player = game.get_player(event.player_index)
+		local demo_state = get_demo_state(building_data.entity.name)
+		local reaction_reactant =
+			next(building_data.chest_inventories[reaction_demo_table_reactant_name].get_contents())
+		if event.button == defines.mouse_button_type.right then
+			demo_reaction_with_reactant(building_data, demo_state, element, reaction_demo_table_reactant_name, nil)
+		elseif player.cursor_stack and player.cursor_stack.valid_for_read then
+			demo_reaction_with_reactant(
+				building_data, demo_state, element, reaction_demo_table_reactant_name, player.cursor_stack.name)
+		elseif reaction_reactant then
+			demo_reaction_with_reactant(
+				building_data, demo_state, element, reaction_demo_table_reactant_name, reaction_reactant)
 		end
 		return
 	end
@@ -245,25 +274,17 @@ local function on_gui_elem_changed(event)
 	local element = event.element
 	local building_data = global.molecule_reaction_building_data[global.current_gui_entity[event.player_index]]
 
-	local reaction_table_selector_reactant = REACTION_TABLE_SELECTOR_NAME_MAP[element.name]
-	if reaction_table_selector_reactant then
-		building_data.reaction.selectors[reaction_table_selector_reactant] = element.elem_value
+	local reaction_table_selector_reactant_name = REACTION_TABLE_SELECTOR_NAME_MAP[element.name]
+	if reaction_table_selector_reactant_name then
+		building_data.reaction.selectors[reaction_table_selector_reactant_name] = element.elem_value
 		entity_assign_cache(building_data, BUILDING_DEFINITIONS[building_data.entity.name])
 		return
 	end
 
-	local reaction_demo_table_reactant = REACTION_DEMO_TABLE_REACTANT_NAME_MAP[element.name]
-	if reaction_demo_table_reactant then
+	local reaction_demo_table_selector_reactant_name = REACTION_DEMO_TABLE_SELECTOR_NAME_MAP[element.name]
+	if reaction_demo_table_selector_reactant_name then
 		local demo_state = get_demo_state(building_data.entity.name)
-		demo_state.reactants[reaction_demo_table_reactant] = element.elem_value
-		demo_reaction(building_data, demo_state, element.parent)
-		return
-	end
-
-	local reaction_demo_table_selector_reactant = REACTION_DEMO_TABLE_SELECTOR_NAME_MAP[element.name]
-	if reaction_demo_table_selector_reactant then
-		local demo_state = get_demo_state(building_data.entity.name)
-		demo_state.selectors[reaction_demo_table_selector_reactant] = element.elem_value
+		demo_state.selectors[reaction_demo_table_selector_reactant_name] = element.elem_value
 		demo_reaction(building_data, demo_state, element.parent)
 		return
 	end
