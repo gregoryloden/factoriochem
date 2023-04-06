767855e7883f71849d1e673b16d32bcd5f33e54e
Gregory Loden <gregory.loden@gmail.com>
2023-04-06 17:28:53 -0400

    add target selectors

--- data/selectors.lua
+++ data/selectors.lua
@@ -1,6 +1,7 @@
 -- Constants
 local SELECTOR_ICON_ROOT = GRAPHICS_ROOT.."selectors/"
 local ROTATION_SELECTOR_SUBGROUP = MOLECULE_REACTION_SELECTOR_PREFIX..ROTATION_SELECTOR_NAME
+local TARGET_SELECTOR_SUBGROUP = MOLECULE_REACTION_SELECTOR_PREFIX..TARGET_SELECTOR_NAME
 
 
 -- Rotation
@@ -20,4 +21,32 @@ for val, suffix in ipairs({"l", "f", "r"}) do
 		icon_mipmaps = ITEM_ICON_MIPMAPS,
 		stack_size = 1,
 	}})
+end
+
+
+-- Target
+data:extend({{
+	type = "item-subgroup",
+	name = TARGET_SELECTOR_SUBGROUP,
+	group = "signals",
+	order = "g",
+}})
+for y_scale = 1, 3 do
+	for x_scale = 1, 3 do
+		for y = 0, y_scale - 1 do
+			for x = 0, x_scale - 1 do
+				local name_spec = y_scale..x_scale..y..x
+				data:extend({{
+					type = "item",
+					name = TARGET_SELECTOR_SUBGROUP.."-"..name_spec,
+					subgroup = TARGET_SELECTOR_SUBGROUP,
+					localised_name = {"item-name.molecule-reaction-selector-target", x_scale, y_scale, x, y},
+					icon = SELECTOR_ICON_ROOT..TARGET_SELECTOR_NAME.."-"..name_spec..".png",
+					icon_size = ITEM_ICON_SIZE,
+					icon_mipmaps = ITEM_ICON_MIPMAPS,
+					stack_size = 1,
+				}})
+			end
+		end
+	end
 end
