0fd0a4c7a7d87c9afd831e279ac4f826c141de22
Gregory Loden <gregory.loden@gmail.com>
2023-04-09 20:17:32 -0400

    use goto/continue where relevant

--- control/entity.lua
+++ control/entity.lua
@@ -90,10 +90,10 @@ local function on_built_entity(event)
 		_, settings = settings.silent_revive()
 		local settings_behavior = settings.get_control_behavior()
 		for i, reactant_name in ipairs(MOLECULE_REACTION_REACTANT_NAMES) do
-			if building_definition.selectors[reactant_name] then
-				local signal = settings_behavior.get_signal(i).signal
-				if signal then building_data.reaction.selectors[reactant_name] = signal.name end
-			end
+			if not building_definition.selectors[reactant_name] then goto continue end
+			local signal = settings_behavior.get_signal(i).signal
+			if signal then building_data.reaction.selectors[reactant_name] = signal.name end
+			::continue::
 		end
 	else
 		settings = entity.surface.create_entity({
