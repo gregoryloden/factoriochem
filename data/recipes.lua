8cfc5efc0a17a094dd3034cf2b3f7bc441f8a365
Gregory Loden <gregory.loden@gmail.com>
2023-04-14 20:45:04 -0400

    set common shared recipe properties in a loop for moleculify recipes

--- data/recipes.lua
+++ data/recipes.lua
@@ -14,46 +14,42 @@ data:extend({
 		type = "recipe-category",
 		name = MOLECULIFY_RECIPE_CATEGORY,
 	},
-	{
-		type = "recipe",
-		name = MOLECULIFY_PREFIX.."water",
-		category = MOLECULIFY_RECIPE_CATEGORY,
-		subgroup = MOLECULIFY_SUBGROUP_NAME,
-		order = "a",
-		ingredients = {{name = "water", amount = 1, type = "fluid"}},
-		results = {
-			{name = MOLECULE_ITEM_PREFIX.."O1-H|1H", amount = 1, probability = 0.75},
-			{name = MOLECULE_ITEM_PREFIX.."H1-O|-1H", amount = 1, probability = 0.05},
-			{name = MOLECULE_ITEM_PREFIX.."-H|H1-1O", amount = 1, probability = 0.05},
-			{name = MOLECULE_ITEM_PREFIX.."H|1O1-H", amount = 1, probability = 0.05},
-			{name = MOLECULE_ITEM_PREFIX.."H|1O|1H", amount = 1, probability = 0.05},
-			{name = MOLECULE_ITEM_PREFIX.."H1-O1-H", amount = 1, probability = 0.05},
-		},
-		icon = GRAPHICS_ROOT.."recipes/"..MOLECULIFY_PREFIX.."water.png",
-		icon_size = 64,
-		icon_mipmaps = 4,
-		energy_required = 1,
-	},
-	{
-		type = "recipe",
-		name = MOLECULIFY_PREFIX.."air",
-		category = MOLECULIFY_RECIPE_CATEGORY,
-		subgroup = MOLECULIFY_SUBGROUP_NAME,
-		order = "b",
-		ingredients = {},
-		results = {
-			{name = MOLECULE_ITEM_PREFIX.."N3-N", amount = 1, probability = 0.375},
-			{name = MOLECULE_ITEM_PREFIX.."N|3N", amount = 1, probability = 0.375},
-			{name = MOLECULE_ITEM_PREFIX.."O2-O", amount = 1, probability = 0.125},
-			{name = MOLECULE_ITEM_PREFIX.."O|2O", amount = 1, probability = 0.125},
-		},
-		icon = GRAPHICS_ROOT.."recipes/"..MOLECULIFY_PREFIX.."air.png",
-		icon_size = 64,
-		icon_mipmaps = 4,
-		energy_required = 1,
-	},
 })
+local moleculify_recipes = {{
+	name = "water",
+	order = "a",
+	ingredients = {{name = "water", amount = 1, type = "fluid"}},
+	results = {
+		{name = MOLECULE_ITEM_PREFIX.."O1-H|1H", amount = 1, probability = 0.75},
+		{name = MOLECULE_ITEM_PREFIX.."H1-O|-1H", amount = 1, probability = 0.05},
+		{name = MOLECULE_ITEM_PREFIX.."-H|H1-1O", amount = 1, probability = 0.05},
+		{name = MOLECULE_ITEM_PREFIX.."H|1O1-H", amount = 1, probability = 0.05},
+		{name = MOLECULE_ITEM_PREFIX.."H|1O|1H", amount = 1, probability = 0.05},
+		{name = MOLECULE_ITEM_PREFIX.."H1-O1-H", amount = 1, probability = 0.05},
+	},
+}, {
+	name = "air",
+	order = "b",
+	ingredients = {},
+	results = {
+		{name = MOLECULE_ITEM_PREFIX.."N3-N", amount = 1, probability = 0.375},
+		{name = MOLECULE_ITEM_PREFIX.."N|3N", amount = 1, probability = 0.375},
+		{name = MOLECULE_ITEM_PREFIX.."O2-O", amount = 1, probability = 0.125},
+		{name = MOLECULE_ITEM_PREFIX.."O|2O", amount = 1, probability = 0.125},
+	},
+}}
+for _, moleculify_recipe in ipairs(moleculify_recipes) do
+	moleculify_recipe.type = "recipe"
+	moleculify_recipe.category = MOLECULIFY_RECIPE_CATEGORY
+	moleculify_recipe.subgroup = MOLECULIFY_SUBGROUP_NAME
+	moleculify_recipe.icon = GRAPHICS_ROOT.."recipes/"..MOLECULIFY_PREFIX..moleculify_recipe.name..".png"
+	moleculify_recipe.icon_size = ITEM_ICON_SIZE
+	moleculify_recipe.icon_mipmaps = ITEM_ICON_MIPMAPS
+	moleculify_recipe.energy_required = 1
+	moleculify_recipe.name = MOLECULIFY_PREFIX..moleculify_recipe.name
+end
 
+data:extend(moleculify_recipes)
 
 -- Science recipes
 data.raw.recipe["automation-science-pack"].ingredients = {
