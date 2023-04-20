fd2af469f3f4abd9f4fed02514d36100ffd3003f
Gregory Loden <gregory.loden@gmail.com>
2023-04-20 13:35:13 -0400

    add (non-functional) ticks-per-update settings

---
+++ settings.lua
@@ -0,0 +1,20 @@
+data:extend({
+	{
+		type = "int-setting",
+		name = "factoriochem-building-ticks-per-update",
+		setting_type = "runtime-global",
+		default_value = 10,
+		minimum_value = 1,
+		maximum_value = 60,
+		order = "a"
+	},
+	{
+		type = "int-setting",
+		name = "factoriochem-detector-ticks-per-update",
+		setting_type = "runtime-global",
+		default_value = 1,
+		minimum_value = 1,
+		maximum_value = 60,
+		order = "b"
+	},
+})
