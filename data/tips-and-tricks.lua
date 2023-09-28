abcd37c1bd76b0418b77b07fef69fccaf1983628
Gregory Loden <gregory.loden@gmail.com>
2023-09-28 15:49:11 -0400

    refactor adding tips-and-tricks entries to a recursive function for ease of adding entries

--- data/tips-and-tricks.lua
+++ data/tips-and-tricks.lua
@@ -3,46 +3,46 @@ local TIPS_AND_TRICKS_GRAPHICS_ROOT = GRAPHICS_ROOT.."tips-and-tricks/"
 
 
 -- Tips and tricks
-data:extend({
-	{
-		type = "tips-and-tricks-item-category",
-		name = "factoriochem",
-		order = "_",
-	},
-	{
+function add_tips_and_tricks_recursive(structure, indent, order)
+	-- add this entry
+	local name = structure.name
+	local entry = {
 		type = "tips-and-tricks-item",
-		name = "factoriochem-introduction",
+		name = "factoriochem-"..name,
 		category = "factoriochem",
-		order = "a",
+		order = order,
+		image = TIPS_AND_TRICKS_GRAPHICS_ROOT..name..".png",
+		starting_status = structure.starting_status or "unlocked",
+	}
+	if indent == 0 then
+		entry.is_title = true
+	else
+		entry.indent = indent
+	end
+	data:extend({entry})
+
+	-- add children entries
+	local children = structure.children
+	if children then
+		for i, child in ipairs(children) do
+			add_tips_and_tricks_recursive(child, indent + 1, order..string.char(i + 96))
+		end
+	end
+end
+
+local tips_and_tricks_structure = {
+	name = "introduction",
+	starting_status = "suggested",
+	children = {{
+		name = "buildings",
 		starting_status = "suggested",
-		is_title = true,
-		image = TIPS_AND_TRICKS_GRAPHICS_ROOT.."introduction.png",
-	},
-	{
-		type = "tips-and-tricks-item",
-		name = "factoriochem-buildings",
-		category = "factoriochem",
-		order = "aa",
-		starting_status = "suggested",
-		indent = 1,
-		image = TIPS_AND_TRICKS_GRAPHICS_ROOT.."buildings.png",
-	},
-	{
-		type = "tips-and-tricks-item",
-		name = "factoriochem-moleculifier",
-		category = "factoriochem",
-		order = "aaa",
-		starting_status = "unlocked",
-		indent = 2,
-		image = TIPS_AND_TRICKS_GRAPHICS_ROOT.."moleculifier.png",
-	},
-	{
-		type = "tips-and-tricks-item",
-		name = "factoriochem-gui",
-		category = "factoriochem",
-		order = "ab",
-		starting_status = "unlocked",
-		indent = 1,
-		image = TIPS_AND_TRICKS_GRAPHICS_ROOT.."gui.png",
-	},
-})
+		children = {{
+			name = "moleculifier",
+		}},
+	}, {
+		name = "gui",
+	}},
+}
+
+data:extend({{type = "tips-and-tricks-item-category", name = "factoriochem", order = "_"}})
+add_tips_and_tricks_recursive(tips_and_tricks_structure, 0, "a")
